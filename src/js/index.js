87ec2f4c5359c5eef75eae5b07c3ff7e6adb82c2
fedodd <fedodd.s@gmail.com>
2018-05-07 21:40:52 +0300

    add maps

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import toggle from './toggle';
+import mapRussia from './mapRussia';
 /* import hover from './hover'; */
 
 const headerToggleBlock = ['cityBlock-wrapper', 'cityActive', 'cityActiveWrapper'];
@@ -11,3 +12,41 @@ toggle('cityClose', headerToggleBlock);
 const academyToggleBlock = ['academyPopup', 'academyPopupBtn', 'academySliderDots', 'academySlider'];
 toggle('academyPopupBtn', academyToggleBlock);
 toggle('academyPopupClose', academyToggleBlock);
+
+let map;
+function initMap() {
+  let uluru = { lat: 61.521958, lng: 93.51359 };
+  map = new google.maps.Map(document.getElementById('mapRussia'), {
+    center: uluru,
+    zoom: 4,
+    styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
+  });
+  let marker = new google.maps.Marker({
+    position: { lat: 55.755826, lng: 37.6173 },
+    map: map,
+    title: 'Moscow',
+    icon: {
+      url: "./../img/mapMarker.svg",
+      scaledSize: new google.maps.Size(32, 32)
+    }
+  });
+}
+
+let mapMoscow;
+function initMap() {
+  let uluru = { lat: 55.755826, lng: 37.6173 };
+  mapMoscow = new google.maps.Map(document.getElementById('mapMoscow'), {
+    center: uluru,
+    zoom: 11,
+    styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
+  });
+  let marker = new google.maps.Marker({
+    position: { lat: 55.788543, lng: 37.527704 },
+    map: mapMoscow,
+    title: 'Moscow',
+    icon: {
+      url: "./../img/mapMarker.svg",
+      scaledSize: new google.maps.Size(32, 32)
+    }
+  });
+}
